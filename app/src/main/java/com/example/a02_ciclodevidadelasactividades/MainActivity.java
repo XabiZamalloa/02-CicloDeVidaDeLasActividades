bec2b4f3f345c6a196dfcd325cdff357736de137
Xabi Zamalloa <xabi@iMac-20.local>
2022-09-26 12:48:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a02_ciclodevidadelasactividades/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.a02_ciclodevidadelasactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    // PASO 1. Variables de la Vista
+    private Button btnAbrir;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("ESTADOS","6- Ejecuto onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("ESTADOS","5- Ejecuto onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("ESTADOS","4- Ejecuto onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("ESTADOS","3- Ejecuto onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("ESTADOS","2- Ejecuto onRestart");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("ESTADOS","1- Ejecuto onCreate");
+
+        btnAbrir=findViewById(R.id.btnAbrirMain);
+        btnAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                    Vehículo Conductor -> Intent (El objeto encargado de avisar a Android:
+                        1. Quien quiere abrir una nueva activity
+                        2. Qué Activity quiere abrir)
+                 */
+                //Tengo 2 tipos:
+                // -Implicitos => Abren Actividades del Sistema (Cámara, Galeria, Contactos, Telefono...)
+                // -Explicitos => Actividad Propia
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
